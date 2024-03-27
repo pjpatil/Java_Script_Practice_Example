f92019071f2d6c40c6f7920c2834a939d09ad236
Pankaj j Patil <pp892810@gmail.com>
2024-03-27 10:30:01 +0530

    looping Examples.

---
+++ forLoop.js
@@ -0,0 +1,7 @@
+// repet this statement 5 times.
+// initialize,condition,increment 
+
+for(let i=1;i<=5;i++)
+{
+    console.log("hiii",i);
+}
---
+++ whileLoop.js
@@ -0,0 +1,15 @@
+
+let i=1;
+
+// while(i<=5){
+//     console.log("hii");
+//     i++;
+// }
+
+
+do{
+    console.log("Hello")
+    i++;
+}while(i<=5);
+
+console.log(i);
