a32d68e601df9dc902ca7973d51b0818152f5197
Pankaj j Patil <pp892810@gmail.com>
2024-04-09 13:41:28 +0530

    aaray methods add.

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,12 +1,13 @@
-let data = [2,5,7,8,9];
+let data = [2,5,7,8,9,11,15];
 
 console.log(data.length);  // it show aaray length
 
 console.log(data.push(10));  // is add data in last position
-
 console.log(data.pop());      // is remove data in last element in the array
 
 console.log(data.shift());   // it is remove fist element in array
-
 console.log(data.unshift(10));    // is add fisst element in the aaray.
+
+console.log(data.splice(2,3,1)); // .splite(x,y,z) x is index value and y is no. of values ypu want to remove and z is add the element. 
+
 console.log(data);
