0a9beb28a994ae1ac860649023d35f2e059c4a81
Pankaj j Patil <pp892810@gmail.com>
2024-04-09 14:06:47 +0530

    aaray loops examples.

---
+++ arrayLoopings.js
@@ -0,0 +1,16 @@
+let nums = [];
+
+nums[0] = 2;
+nums[50] = 60;
+
+// console.log(nums);  // [ 2, <49 empty items>, 60 ]
+
+for (let n of nums) {
+    console.log(n);
+}
+
+
+for (let key in nums) {
+    console.log(nums[key]);
+}
+
