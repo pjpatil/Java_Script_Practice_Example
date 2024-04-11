64dc9ff263293f2099d169ef3cb39869d7618560
Pankaj j Patil <pp892810@gmail.com>
2024-04-11 10:54:42 +0530

    aaray with destructuring ,for each loops examples.

---
+++ arrayDestructuring.js
@@ -0,0 +1,17 @@
+
+let nums= [5,7,2,4];
+console.log(nums);
+
+let [a,b,c,d]= nums; // a=5,b=7,c=2,d=4
+
+console.log(c);
+
+
+/*
+let words="My name is Pnakaj Patil Im from jalgaon".split(' ');
+let [a,b,...d]=words;
+
+console.log(d);
+ console.log(d.length);
+*/
+
---
+++ forEachLoopArrayMethod.js
@@ -0,0 +1,7 @@
+let nums = [42, 52, 31, 24, 82, 98, 12];
+
+nums.forEach((n,i,nums) => {
+    console.log(n,i,nums);
+});
+
+// console.log(nums);
